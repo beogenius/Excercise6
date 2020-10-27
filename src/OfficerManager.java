42e32746bce35731a20cdb35bc617f1da8625e11
beogenius <70867972+beogenius@users.noreply.github.com>
2020-10-27 22:42:29 +0700

    update excercise 6

---
+++ src/OfficerManager.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OfficerManager {
+    private List<Officer> officers;
+
+    public OfficerManager() {
+        this.officers = new ArrayList<>();
+    }
+
+    public void addOfficer(Officer officer) {
+        this.officers.add(officer);
+    }
+
+    public List<Officer> searchOfficerByName(String name) {
+        return this.officers.stream().filter(o -> o.getName().contains(name)).collect(Collectors.toList());
+    }
+
+    public void showListInforOfficer() {
+        this.officers.forEach(o -> System.out.println(o.toString()));
+    }
+}
